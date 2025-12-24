ebac8fd78abea2741c2622a2e97e21f61f9205b9
Trenton <150291395+Tc00k@users.noreply.github.com>
2025-12-24 17:05:40 -0600

    Update jamfGitSync.sh
    V1.1
    
    - Adjusted the retrieval functions to utilize the jamfScriptBuild variable to reduce the amount of API calls, this has reduced run time from above a minute to less than ten seconds on average
    
    - Added functions to support downloading missing scripts from Jamf and uploading newly created local scripts to Jamf. That way regardless of how a new script is added it will end up synced across GitHub and your Jamf instance

--- jamfGitSync.sh
+++ jamfGitSync.sh
@@ -1,272 +1,334 @@
 #!/bin/bash
 
 ## Created by Trenton Cook
-## Mass updates scripts in your Jamf Pro instance to match your locally hosted Git repository
+## Created on 2025-12-22
+## Version 1.1
 
-###############
-## VARIABLES ##
-###############
-
-## When set to 'true' this will disable the uploadToJamf function for testing purposes
-debugMode="false"
+## VARIABLES
 
 ## DO NOT CHANGE
 scriptURL="$JAMF_URL/api/v1/scripts"
 startTime=$(date +%s)
 
-###############
-## FUNCTIONS ##
-###############
+## CHANGE IF NEEDED
+debugMode="true"
 
-## Bearer token retrieval function
+## FUNCTIONS
+
+debug() {
+    if [[ "$debugMode" == "true" ]]; then
+        echo $@
+    fi
+}
+
+normalizeKey() {
+    echo "$1" | tr '[:upper:]' '[:lower:]' \
+    | tr ' ' '_' \
+    | tr -d '(' \
+    | tr -d ')' \
+    | tr -d '{' \
+    | tr -d '}' \
+    | tr -d '[' \
+    | tr -d ']' \
+    | tr -d '"' \
+    | tr -d "'"
+}
+
+normalizeContent() {
+    local contentToNormalize="$1"
+    echo "$contentToNormalize" | tr -d '\r' | sed 's/[[:space:]]*$//'
+}
+
 getBearerToken() {
     local responseBody
     responseBody=$(curl -s --location --request POST "$JAMF_URL/api/oauth/token" \
     --header 'Content-Type: application/x-www-form-urlencoded' \
-    --data-urlencode "client_id=$JAMF_CLIENT_ID" \
+    --data-urlencode "client_id=$CLIENT_ID" \
     --data-urlencode 'grant_type=client_credentials' \
-    --data-urlencode "client_secret=$JAMF_CLIENT_SECRET" \
+    --data-urlencode "client_secret=$CLIENT_SECRET" \
     )
 
     bearerToken=$(echo $responseBody | jq -r '.access_token // empty')
 
     if [[ -z "$bearerToken" ]]; then
-        echo "FAILURE: Bearer token could not be retrieved"
+        debug "FAILURE: Bearer token could not be retrieved"
         exit 1
+    else
+        debug "Bearer Token retrieved successfully"
     fi
 }
 
-## Bearer token invalidation function
 invalidateToken() {
     local responseBody
 
     responseBody=$(curl -w "%{http_code}" -H "Authorization: Bearer ${bearerToken}" $JAMF_URL/api/v1/auth/invalidate-token -X POST -s -o /dev/null)
 
     if [[ ${responseBody} != 204 ]]; then
-        echo "FAILURE: Bearer token could not be invalidated"
+        debug "FAILURE: Bearer token could not be invalidated"
         return 1
+    else
+        debug "Bearer Token invalidated successfully"
     fi
 
     bearerToken=""
 }
 
-## Retrieve local script contents function
-retrieveLocalScriptContent() {
-    local localScriptPath="$WORKING_DIRECTORY/$1.sh"
+buildLocalInventory() {
+    declare -gA localInventory
+    debug "-- Building local inventory --"
 
-    if [[ ! -f "$localScriptPath" ]]; then
-        echo "Script $1 not found in working directory"
-        return
-    fi
+    for script in "$WORKING_DIRECTORY"/*.sh; do
+        [[ -f "$script" ]] || continue
+        localScriptName="$(basename "$script" .sh)"
+        nameKey=$(normalizeKey "$localScriptName")
+        localInventory["$nameKey"]="$script"
 
-    localScriptContent=$(cat "$localScriptPath")
-    localScriptContent=$(echo "$localScriptContent" | tr -d '\r' | sed 's/[[:space:]]*$//')
+        debug "Added $localScriptName to localInventory"
+    done
+
+    debug "-- Local inventory built --"
 }
 
-## Retrieve Jamf script contents function
-retrieveJamfScriptContent() {
-    local scriptName=$1
-    local script_ID="${2:-}"
-    local combinations=("$scriptName" "${scriptName// /_}" "${scriptName//_/ }")
-    local totalCount
-    local retrievedCount
+buildJamfInventory() {
+    declare -gA jamfInventory
+    debug "-- Building Jamf inventory --"
+    debug "-- Retrieving Jamf scripts --"
+    jamfScriptBuild=$(curl -s -H "Authorization: Bearer ${bearerToken}" "$scriptURL?page=0&page-size=2000")
 
-    if [[ -z "$script_ID" ]]; then
-        while true; do
-            for nameVariant in "${combinations[@]}"; do
-                script_ID=$( echo "$scriptResponse" | jq -r --arg script_name "$nameVariant" '.results[] | select(.name == $script_name) | .id')
-                if [[ -n "$script_ID" ]]; then
-                    break 2
+    while IFS=$'\t' read -r name id; do
+        key=$(normalizeKey "$name")
+        jamfInventory["$key"]="$id"
+        debug "Added $name to jamfInventory"
+    done < <(
+        echo "$jamfScriptBuild" | jq -r '.results[] | "\(.name)\t\(.id)"'
+    )
+
+    debug "-- Jamf inventory built --"
+}
+
+compareInventories() {
+    debug "-- Comparing inventories --"
+
+    local changesFound="false"
+    local allKeys=()
+
+    # Collect all keys
+    for k in "${!localInventory[@]}"; do allKeys+=("$k"); done
+    for k in "${!jamfInventory[@]}"; do allKeys+=("$k"); done
+    readarray -t allKeys < <(printf "%s\n" "${allKeys[@]}" | sort -u)
+
+    for key in "${allKeys[@]}"; do
+        local localVal="${localInventory[$key]:-<missing>}"
+        local jamfVal="${jamfInventory[$key]:-<missing>}"
+
+        if [[ "$localVal" == "<missing>" ]]; then
+            echo ""
+            echo "Missing locally: $key"
+            echo "  Jamf ID: $jamfVal"
+            downloadFromJamf "$key"
+            changesFound="true"
+        elif [[ "$jamfVal" == "<missing>" ]]; then
+            echo ""
+            echo "Missing in Jamf: $key"
+            echo "  Local Path: $localVal"
+            uploadToJamf "$key"
+            changesFound="true"
+        else
+            retrieveLocalScriptContent "$key"
+            retrieveJamfScriptContent "$key"
+
+            if [[ "$localScriptContent" != "$jamfScriptContent" ]]; then
+                echo ""
+                echo "Content mismatch: $key"
+                echo "  Local Path: $localVal"
+                echo "  Jamf ID:    $jamfVal"
+                changesFound="true"
+
+                if [[ "$debugMode" == "true" ]]; then
+                    debug "Would have updated Jamf with local script '$key'"
+                else
+                    updateJamf "$key"
                 fi
-            done
-
-            totalCount=$( echo "$scriptResponse" | jq -r '.totalCount' )
-            retrievedCount=$( echo "$scriptResponse" | jq -r '.results | length' )
-            if [[ $retrievedCount -ge $totalCount ]]; then
-                break
             fi
-        done
+        fi
+    done
+
+    if [[ "$changesFound" != "true" ]]; then
+        echo "No mismatches or missing scripts detected"
     fi
 
-    if [[ -z "$script_ID" ]]; then
-        echo "WARN: '$scriptName' not found in Jamf, skipping"
+    debug "-- Inventory comparison complete --"
+}
+
+downloadFromJamf() {
+    local key="$1"
+    local scriptID="${jamfInventory[$key]}"
+    local filePath="$WORKING_DIRECTORY/$key.sh"
+
+    if [[ -z "$scriptID" ]]; then
+        debug "Cannot download $key: No Jamf ID found"
         return 1
     fi
 
-    jamfScriptObject=$( curl -s -X 'GET' "$scriptURL/$script_ID" -H 'accept: application/json' -H "Authorization: Bearer $bearerToken" )
+    if [[ "$debugMode" == "true" ]]; then
+        debug "Would have downloaded script '$key' from Jamf (ID: $scriptID) to $filePath"
+        return 0
+    fi
 
-    ## Extract all values and assign to individual variables
-    name=$(echo "$jamfScriptObject" | jq -r '.name')
-    info=$(echo "$jamfScriptObject" | jq -r '.info')
-    notes=$(echo "$jamfScriptObject" | jq -r '.notes')
-    priority=$(echo "$jamfScriptObject" | jq -r '.priority')
-    categoryId=$(echo "$jamfScriptObject" | jq -r '.categoryId')
-    parameter4=$(echo "$jamfScriptObject" | jq -r '.parameter4')
-    parameter5=$(echo "$jamfScriptObject" | jq -r '.parameter5')
-    parameter6=$(echo "$jamfScriptObject" | jq -r '.parameter6')
-    parameter7=$(echo "$jamfScriptObject" | jq -r '.parameter7')
-    parameter8=$(echo "$jamfScriptObject" | jq -r '.parameter8')
-    parameter9=$(echo "$jamfScriptObject" | jq -r '.parameter9')
-    parameter10=$(echo "$jamfScriptObject" | jq -r '.parameter10')
-    parameter11=$(echo "$jamfScriptObject" | jq -r '.parameter11')
-    osRequirements=$(echo "$jamfScriptObject" | jq -r '.osRequirements')
-    jamfScriptContent=$(echo "$jamfScriptObject" | jq -r '.scriptContents' | tr -d '\r' | sed 's/[[:space:]]*$//')
+    debug "Downloading script '$key' from Jamf (ID: $scriptID) to $filePath"
+
+    jamfScriptContent=$(curl -s -H "Authorization: Bearer ${bearerToken}" \
+        "$scriptURL/$scriptID" | jq -r '.scriptContents // empty')
+
+    if [[ -z "$jamfScriptContent" ]]; then
+        echo "Failed to retrieve content for $key from Jamf"
+        return 1
+    fi
+
+    printf "%s\n" "$jamfScriptContent" > "$filePath"
+    echo "Downloaded $key from Jamf to $filePath"
+}
+
+uploadToJamf() {
+    local key="$1"
+    local localPath="${localInventory[$key]}"
+
+    if [[ ! -f "$localPath" ]]; then
+        debug "Cannot upload $key: Local file not found at $localPath"
+        return 1
+    fi
+
+    if [[ "$debugMode" == "true" ]]; then
+        debug "Would have uploaded script '$key' from $localPath to Jamf"
+        return 0
+    fi
+
+    debug "Uploading script '$key' from $localPath to Jamf"
+
+    local localScriptContent
+    localScriptContent=$(cat "$localPath")
+
+    # Escape for JSON
+    local escapedContent
+    escapedContent=$(jq -Rs '.' <<< "$localScriptContent")
+
+    curl -s -X POST "$scriptURL" \
+        -H "Authorization: Bearer ${bearerToken}" \
+        -H "Content-Type: application/json" \
+        -d @- <<-EOF
+    {
+        "name": "$key",
+        "info": "",
+        "notes": "",
+        "priority": "AFTER",
+        "categoryId": "1",
+        "parameter4": "",
+        "parameter5": "",
+        "parameter6": "",
+        "parameter7": "",
+        "parameter8": "",
+        "parameter9": "",
+        "parameter10": "",
+        "parameter11": "",
+        "osRequirements": "",
+        "scriptContents": $escapedContent
+    }
+	EOF
+
+    echo "Uploaded $key from $localPath to Jamf"
+}
+
+updateJamf() {
+    local key="$1"
+    local scriptID="${jamfInventory[$key]}"
+    local localPath="${localInventory[$key]}"
+
+    if [[ -z "$scriptID" ]]; then
+        debug "Cannot update $key: Jamf ID not found"
+        return 1
+    fi
+
+    if [[ ! -f "$localPath" ]]; then
+        debug "Cannot update $key: Local file not found at $localPath"
+        return 1
+    fi
+
+    local localScriptContent
+    localScriptContent=$(cat "$localPath")
+    localScriptContent=$(normalizeContent "$localScriptContent")
+
+    local escapedContent
+    escapedContent=$(jq -Rs '.' <<< "$localScriptContent")
+
+    if [[ "$debugMode" == "true" ]]; then
+        debug "Would have updated Jamf script '$key' (ID: $scriptID) from $localPath"
+        return 0
+    fi
+
+    curl -s -X PUT "$scriptURL/$scriptID" \
+        -H "Authorization: Bearer ${bearerToken}" \
+        -H "Content-Type: application/json" \
+        -d @- <<-EOF
+    {
+        "name": "$key",
+        "info": "",
+        "notes": "",
+        "priority": "AFTER",
+        "categoryId": "1",
+        "parameter4": "",
+        "parameter5": "",
+        "parameter6": "",
+        "parameter7": "",
+        "parameter8": "",
+        "parameter9": "",
+        "parameter10": "",
+        "parameter11": "",
+        "osRequirements": "",
+        "scriptContents": $escapedContent
+    }
+	EOF
+
+    echo "Updated Jamf script '$key' (ID: $scriptID) from $localPath"
+}
+
+retrieveLocalScriptContent() {
+    local key="$1"
+    local localScriptPath="$WORKING_DIRECTORY/$key.sh"
+
+    if [[ ! -f "$localScriptPath" ]]; then
+        debug "Local script not found for key: $key"
+        localScriptContent=""
+        return 1
+    fi
+
+    localScriptContent=$(<"$localScriptPath")
+    localScriptContent=$(normalizeContent "$localScriptContent")
+}
+
+retrieveJamfScriptContent() {
+    local key="$1"
+    local script_ID="${jamfInventory[$key]:-}"
+
+    if [[ -z "$script_ID" ]]; then
+        debug "WARN: No Jamf ID found for key '$key'"
+        jamfScriptContent=""
+        return 1
+    fi
+
+    jamfScriptContent=$(echo "$jamfScriptBuild" | \
+        jq -r --arg id "$script_ID" '.results[] | select(.id | tostring == $id) | .scriptContents // empty')
+
+    jamfScriptContent=$(normalizeContent "$jamfScriptContent")
 
     scriptID="$script_ID"
 }
 
-## Push updates to Jamf scripts function
-updateScript() {
-    local scriptNameTotal="$1"
-    local scriptID="$2"
-    local escapedLocalScriptContent
-
-    retrieveJamfScriptContent "$scriptNameTotal" "$scriptID"
-    retrieveLocalScriptContent "$scriptNameTotal"
-
-    escapedLocalScriptContent=$(jq -Rs '.' <<<"$localScriptContent")
-
-    if [[ "$debugMode" == "true" ]]; then
-        echo "Script $scriptNameTotal would be updated"
-    else
-        curl -s -o /dev/null -X 'PUT' \
-            "${scriptURL}/${scriptID}" \
-            -H 'accept application/json' \
-            -H "Authorization: Bearer $bearerToken" \
-            -H "Content-Type: application/json" \
-            -d @- <<-EOF
-		{
-		    "name": "$name",
-		    "info": "$info",
-		    "notes": "$notes",
-		    "priority": "$priority",
-		    "categoryId": "$categoryId",
-		    "parameter4": "$parameter4",
-		    "parameter5": "$parameter5",
-		    "parameter6": "$parameter6",
-		    "parameter7": "$parameter7",
-		    "parameter8": "$parameter8",
-		    "parameter9": "$parameter9",
-		    "parameter10": "$parameter10",
-		    "parameter11": "$parameter11",
-		    "osRequirements": "$osRequirements",
-		    "scriptContents": $escapedLocalScriptContent
-		}
-		EOF
-
-        echo "Script $scriptNameTotal updated successfully"
-    fi
-}
-
-WORKING_DIRECTORYCheck() {
-    if [[ -d "${WORKING_DIRECTORY}" ]]; then
-        echo "Working directory ${WORKING_DIRECTORY} exists"
-        return 0
-    fi
-
-    echo "Working directory ${WORKING_DIRECTORY} does not exist"
-    mkdir -p "$WORKING_DIRECTORY"
-    if [[ $? -ne 0 ]]; then
-        echo "Failed to create working directory ${WORKING_DIRECTORY}"
-        exit 1
-    fi
-}
-
-###############################
-## CHECK FOR MISSING SCRIPTS ##
-###############################
-
-jamfScriptCount() {
-    echo "$scriptResponse" | jq -r '.results[] | "\(.id)\t\(.name)"'
-}
-
-downloadMissing() {
-    echo "Downloading any missing scripts from Jamf to local directory"
-
-    jamfScriptCount | while IFS=$'\t' read -r scriptID scriptName; do
-        [[ -z "${scriptID:-}" || -z "${scriptName:-}" ]] && continue
-
-        local fileBase
-        local outFile
-        fileBase=$(echo "$scriptName" | sed -E 's/[[:space:]]+/_/g; s|/|_|g')
-        outFile="${WORKING_DIRECTORY}/${fileBase}.sh"
-
-        if [[ -f "$outFile" ]]; then
-            continue
-        fi
-
-        echo "Downloading: ${scriptName}"
-
-        local detail
-        local body
-        detail="$(curl -s -H "Authorization: Bearer ${bearerToken}" "${scriptURL}/${scriptID}")"
-        body="$(echo "$detail" | jq -r '.scriptContents // .script_contents // .script // empty')"
-
-        if [[ -z "$body" ]]; then
-            echo "WARN: no script body returned for '${scriptName}' (ID: ${scriptID})" >&2
-            continue
-        fi
-
-        scriptName="$(printf '%s' "$scriptName" | tr -d '\r\n')"
-        printf "%s\n" "$body" > "$outFile"
-    done
-}
-
-##################
-## Main Runtime ##
-##################
-
-WORKING_DIRECTORYCheck
 getBearerToken
-
-changesFound="false"
-
-## Gather all scripts from Jamf and their contents once
-echo "Gathering all scripts from Jamf"
-scriptResponse=$(curl -s -H "Authorization: Bearer ${bearerToken}" "$scriptURL?page=0&page-size=2000")
-
-## Initialize associative array for change tracking
-declare -A changedMap
-
-## Iterate through all scripts in local directory and compare to their Jamf counterparts
-echo "Comparing local scripts to Jamf counterparts"
-echo ""
-for script in "$WORKING_DIRECTORY"/*.sh; do
-    echo "Comparing $(basename "$script")"
-    scriptName=$(basename "$script" .sh)
-    ## Gather local script content for comparison
-    retrieveLocalScriptContent "$scriptName"
-    ## Gather network script content for comparison
-    retrieveJamfScriptContent "$scriptName"
-    if [[ "$localScriptContent" != "$jamfScriptContent" ]]; then
-        changedMap["$scriptName"]="$scriptID"
-    fi
-done
-echo ""
-
-## Output any changes found
-if (( ${#changedMap[@]} > 0 )); then
-    changesFound="true"
-    echo "Changes found:"
-    for name in "${!changedMap[@]}"; do
-        echo "- $name"
-    done
-else
-    echo "No changes found"
-fi
-
-## Update changed scripts in Jamf
-if [[ "$changesFound" == "true" ]]; then
-    getBearerToken
-    for name in "${!changedMap[@]}"; do
-        id="${changedMap[$name]}"
-        updateScript "$name" "$id"
-    done
-fi
-
-downloadMissing
+buildLocalInventory
+buildJamfInventory
+compareInventories
 invalidateToken
 
 endTime=$(date +%s)
-elapsedTime=$(( endTime - startTime ))
-echo "Run took: $elapsedTime seconds"
+timeDiff=$(($endTime - $startTime))
+echo "Total time: $timeDiff seconds"
